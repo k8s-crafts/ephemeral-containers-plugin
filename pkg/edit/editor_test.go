2de28ad58f19167313a5c3928fe3a3d32fbf41f8
Thuan Vo <thuan.votann@gmail.com>
2024-10-09 14:14:01 -0700

    test: tests for edit pkg
    Signed-off-by: Thuan Vo <thuan.votann@gmail.com>

--- pkg/edit/editor_test.go
+++ pkg/edit/editor_test.go
@@ -13,3 +13,103 @@
 // limitations under the License.
 
 package edit
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetEditorCmd(t *testing.T) {
+	type Env struct {
+		Key   string
+		Value string
+	}
+
+	setupEnv := func(t *testing.T, envs []Env) {
+		for _, env := range envs {
+			t.Setenv(env.Key, env.Value)
+		}
+	}
+
+	tests := []struct {
+		desciptions string
+		fromFlag    string
+		envs        []Env
+		expected    string
+	}{
+
+		{
+			desciptions: "should return editor cmd from cli flags",
+			fromFlag:    "nano",
+			envs: []Env{
+				{
+					Key:   ENV_EDITOR,
+					Value: "code",
+				},
+				{
+					Key:   ENV_KUBE_EDITOR,
+					Value: "vi",
+				},
+			},
+			expected: "nano",
+		},
+		{
+			desciptions: "should return editor cmd from env var KUBE_EDITOR",
+			fromFlag:    "",
+			envs: []Env{
+				{
+					Key:   ENV_EDITOR,
+					Value: "code",
+				},
+				{
+					Key:   ENV_KUBE_EDITOR,
+					Value: "vi",
+				},
+			},
+			expected: "vi",
+		},
+		{
+			desciptions: "should return editor cmd from env var EDITOR",
+			fromFlag:    "",
+			envs: []Env{
+				{
+					Key:   ENV_EDITOR,
+					Value: "code",
+				},
+			},
+			expected: "code",
+		},
+		{
+			desciptions: "should return vim as default editor cmd",
+			fromFlag:    "",
+			// Env vars are set to empty
+			// since the system might have already set them
+			envs: []Env{
+				{
+					Key:   ENV_EDITOR,
+					Value: "",
+				},
+				{
+					Key:   ENV_KUBE_EDITOR,
+					Value: "",
+				},
+			},
+			expected: "vi",
+		},
+	}
+
+	for _, test := range tests {
+
+		t.Run(test.desciptions, func(t *testing.T) {
+			setupEnv(t, test.envs)
+
+			actual := GetEditorCmd(test.fromFlag)
+			if actual != test.expected {
+				t.Errorf("%v", os.Getenv("KUBE_EDITOR"))
+				t.Fatalf("expect to get %v, but received %v", test.expected, actual)
+
+			}
+		})
+
+	}
+}
