af96970f513714bd3c4a58dd85e92b1547a84414
Thuan Vo <thuan.votann@gmail.com>
2024-10-22 05:57:14 -0700

    chore: run lints

--- pkg/formatter/formatter_test.go
+++ pkg/formatter/formatter_test.go
@@ -19,7 +19,6 @@ import (
 	"github.com/k8s-crafts/ephemeral-containers-plugin/pkg/version"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
